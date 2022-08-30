fba3d5887f23e37cdeb50638407b9b4635fb0216
Miguel Morassuti <miguelguimaraesmorassuti@hotmail.com>
2022-08-30 14:46:26 -0300

    Erro
    Por algum motivo não consegui realizar a instalação do react-dom-router, irei concertar esse código futuramente.

--- REACT/my-app/src/App.js
+++ REACT/my-app/src/App.js
@@ -1,35 +1,17 @@
-import './styles/App.css';
-import Feed from './components/feed.js'; //Import da função feed do arq feed.js
-import { useState } from 'react'; //Função que vai ajudar na hora de recarregar
-//a pagina quando o usuario fizer novos posts
+import { BrowserRouter } from 'react-router-dom';
 
-import PostForm from './components/postForm';
+import './styles/App.css';
+
+import AppRoutes from './Routes'
 
 export default function App() { //Export default = para poder importar no index
-    const [posts, setPosts] = useState([])
 
 
-        function handleSubmit(history, userName){        
-          
-            setPosts([  //criando novos posts
-             ...posts, //Para renderizar os posts feitos no codigo  
-             {
-                 id: Math.random(),      
-                 content: history,
-                 userName:  userName,
-                 publishedAt: new Date()
-             }
-            ])       
-        }
-
-    return(
+    return (
         <div className="wrapper">
-            <PostForm onSubmit={handleSubmit}/> 
-            {/*Enviando função para o postForm.js (callback, crio uma function no arq pai, porem os dados necessarios para ela estao no filho, entao mando a funcao para la ser executada e retornada)*/}
-            
-            <main>
-                <Feed posts={posts} />{/*Feed é um component filho do App.js */}
-            </main>
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
         </div>
     );
 }
